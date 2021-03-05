0831e5d424ac852c2f4d5b2b3ecc3ece9a7ae769
sasabeghiu <70911600+sasabeghiu@users.noreply.github.com>
2021-03-05 21:27:38 +0100

    Created RoomDAO
    created rooomdao

---
+++ SomerenDAL/RoomDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class RoomDao : BaseDao
+    {
+        public List<Room> GetAllRooms()
+        {
+            string query = "SELECT RoomNr, Room_Type, Room_Size FROM [Room]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Room> ReadTables(DataTable dataTable)
+        {
+            List<Room> rooms = new List<Room>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Room room = new Room()
+                {
+                    Number = (int)dr["StudentId"],
+                    Type = (bool)(dr["Room_Type"]),
+                    Capacity = (int)(dr["Room_Size"])
+                };
+                rooms.Add(room);
+            }
+            return rooms;
+        }
+    }
+}
